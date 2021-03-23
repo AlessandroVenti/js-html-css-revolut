117174f9a0f06d5e26672285e17b891c2c5f71e5
Alessandro Venti <alessandro.venti@icloud.com>
2021-03-23 13:40:07 +0100

    setup project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+                              // GOAL: sfruttando quando visto questa mattina a lezione, riprodurre la dropdown (menu in alto) 
+                              // che trovate al sito di Revolut linkato qua sotto.
+                              // NOTA1: e' richiesta la sola dropdown del menu in alto
+                              // NOTA2: come sempre massima priorita' al js rispetto al html/css
+                       
+
+console.log(`test`);
