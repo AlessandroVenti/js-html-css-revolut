786ffd9c5a03094a5f10ce3c918e0a382ef6eb63
Alessandro Venti <alessandro.venti@icloud.com>
2021-03-23 15:27:50 +0100

    hover in done

--- js/script.js
+++ js/script.js
@@ -2,6 +2,43 @@
                               // che trovate al sito di Revolut linkato qua sotto.
                               // NOTA1: e' richiesta la sola dropdown del menu in alto
                               // NOTA2: come sempre massima priorita' al js rispetto al html/css
-                       
 
-console.log(`test`);
+function handlerIn () {
+
+     var $activeLink = $('li').find('.js-link');
+
+     var $activeDropdown = $('li').find('.js-none');
+
+     console.log($activeDropdown);
+
+     console.log($activeLink);
+
+     
+     $activeLink.hover (function() {
+
+          $(this).next('.js-none').removeClass('js-none');
+
+          
+     }), function() {
+
+          $(this).find('.js-none').addClass('js-none');
+
+     }
+
+     
+}
+
+$(document).ready(handlerIn);
+
+
+
+                              
+
+// function final() {
+
+     
+
+
+// }
+
+// $(document).ready(final);
